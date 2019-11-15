d5071bf4a57e1cdd41abbb5a9221253ecbe52a5c
88443 <88443@farnborough.ac.uk>
2019-11-15 15:56:54 +0000

    Sorting out database and files

--- src/main/java/LinesControl.java
+++ src/main/java/LinesControl.java
@@ -1,16 +1,34 @@
 import java.sql.PreparedStatement;
 import java.sql.ResultSet;
+import java.util.Scanner;
 
 public class LinesControl {
+
+    public static void addLine(){
+        Scanner input = new Scanner (System.in);
+        try{
+            PreparedStatement ps = Main.db.prepareStatement("INSERT INTO Lines (TabID, LineID, UserID) VALUES (?,?,?)");
+            ps. setInt(1, TabID);
+            ps.setInt(2, LineID);
+            ps.setString(3, Lyrics);
+            ps.executeUpdate();
+        } catch (Exception exception){
+            System.out.println("Database error");
+        }
+    }
+
+
+
     public static void listLines(){
         try {
-            PreparedStatement ps = Main.db.prepareStatement("SELECT LineID, TabID, Index, Lyrics FROM Lines");
+            PreparedStatement ps = Main.db.prepareStatement("SELECT LineID, TabID, LineIndex, Lyrics FROM Lines");
             ResultSet results = ps.executeQuery();
             while (results.next()) {
                 int LineID = results.getInt(1);
                 int TabID = results.getInt(2);
                 int Index = results.getInt(3);
                 String Lyrics = results.getString(4);
+                System.out.println(LineID + " " + TabID + " " + Index + " " + Lyrics);
             }
 
         } catch (Exception exception) {
